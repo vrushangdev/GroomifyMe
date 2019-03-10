eaa1c3242b108e95a4933f95929b68f18efd14c5
Vrushang Desai <vrushangapple@icloud.com>
2019-03-10 23:38:31 +0530

    Initialization

---
+++ app/src/main/java/com/teambot/groomifyme/MainActivity.java
@@ -0,0 +1,22 @@
+package com.teambot.groomifyme;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        },3000);
+    }
+}
